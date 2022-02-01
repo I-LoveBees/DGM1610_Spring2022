4ceb06d161f56dd6ae18499ea18f06169c3a98a1
Brooke Hinckley <10926137@uvu.edu>
2022-01-31 17:23:59 -0700

    created player movement and controller
    it doesn't seem to actually move, but values do change

--- My first project/Assets/scripts/PlayerControl.cs
+++ My first project/Assets/scripts/PlayerControl.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public float speed = 20.0f;
+    public float turnSpeed = 50.0f;
+    public float hInput; //horizontal input
+    public float fInput; //forward/backward input
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,11 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        hInput = Input.GetAxis("Horizontal");
+        fInput = Input.GetAxis("Vertical");
+
+        // makes player move forward (can be problematic)
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
